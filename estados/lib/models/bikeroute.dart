3733215026033091ef952d2ee7f08bbe0e42d29b
AT1927 <atmunoz1927@gmail.com>
2024-06-04 14:53:34 -0500

    Change model

---
+++ estados/lib/models/bikeroute.dart
@@ -0,0 +1,13 @@
+class Route{
+  String name;
+  double distance;
+  String difficulty;
+  bool completed;
+
+  Route({
+    required this.name,
+    required this.distance,
+    required this. difficulty,
+    required this.completed,
+  });
+}
